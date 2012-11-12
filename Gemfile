721841732356d40f85aab7b4f7a1073a113e3d71
Jeremy Woertink <jeremywoertink@gmail.com>
2012-11-11 22:37:57 -0800

    updated app for use with new tourets gem

--- Gemfile
+++ Gemfile
@@ -3,9 +3,8 @@ source 'https://rubygems.org'
 gem 'rails', '3.2.8'
 gem 'sqlite3'
 gem 'jquery-rails'
-gem 'ruby-rets', :github => 'Placester/ruby-rets'
-#gem 'rets'
-#gem 'rets4r'
+#gem 'ruby-rets', :github => 'Placester/ruby-rets'
+gem 'tourets', :github => 'jwoertink/tourets'
 
 # Gems used only for assets and not required
 # in production environments by default.
--- app/models/property.rb
+++ app/models/property.rb
@@ -1,33 +1,5 @@
 class Property
   
-  def initialize(auth_hash)
-    @client = RETS::Client.login(auth_hash)
-  end
   
-  def search(params = {}, &block)
-    # Find only active properties
-    params.merge!("242" => "ER,EA,C", "1809" => "Y", "130" => "Y")
-    filter = hash_to_rets_str(params)
-    
-    @client.search(:search_type => :Property, :class => "1", :query => filter, &block)
-  end
-  
-  def get_object
-    @client.get_object(:resource => :Property, :type => :Photo, :location => false, :id => "1:0:*") do |object|
-      puts "Object-ID #{object[:headers]['Object-ID']}, Content-ID #{object[:headers]['Content-ID']}, Description #{object[:headers]['Description']}"
-      puts "Data"
-      puts object[:content]
-    end
-  end
-  
-  private
-  
-    def hash_to_rets_str(hash)
-      str = []
-      hash.each_pair do |k,v|
-        str << "(#{k.to_s.camelize}=#{v})"
-      end
-      str.join(',')
-    end
   
 end
